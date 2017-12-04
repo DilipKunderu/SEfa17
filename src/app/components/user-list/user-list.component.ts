50c2ac8b58c009e080daa53771fd0e433a4d4a3e
DilipKunderu <dilipkunderu@gmail.com>
2017-12-03 20:46:15 -0500

    had to push

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, IterableDiffers, DoCheck } from '@angular/core';
 import { HouseListingService } from '../../house-listing.service';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
@@ -9,38 +9,42 @@ import { Router } from '@angular/router';
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent implements OnInit {
+export class UserListComponent implements OnInit, DoCheck {
   res: object;
-  array: Array<any> = [];
+  @Input() array: Array<any> = [];
+  differ: any;
 
   constructor(
     private data: HouseListingService,
     private http: HttpClient,
-    private router: Router) { }
+    private router: Router,
+    private differs: IterableDiffers) { 
+      this.differ = differs.find([]).create(null);
+    }
 
   deleted (s: string) {
     this.http.delete('http://192.168.2.24:3000/delete_id?id='+ s)
     .subscribe(res => {
-      console.log(res);
-      this.router.navigate(['/']);
-      // this.array.length = 0;
-      // this.http.get('http://192.168.2.24:3000/mylease?name=Saptarshi')
-      // .subscribe(res => {
-      //   this.res = res;
-      //   console.log(res);
-      //   [].push.apply(this.array, res);
-        
-      // });
+      // console.log(res);
+      // this.router.navigate(['/']);
     })
   }
 
   ngOnInit() {
+    this.getLatest();
+  }
+
+  ngDoCheck() {
+    const change = this.differ.diff(this.array);
+    this.array.length = 0;
+    this.getLatest();
+  }
+
+  getLatest() {
     this.http.get('http://192.168.2.24:3000/mylease?name=' + this.data.userName)
     .subscribe(res => {
       this.res = res;
-      console.log(res);
       [].push.apply(this.array, res);
     });
   }
-
 }
