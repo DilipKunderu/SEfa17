c987c7227b1f7841661dc276e17db5e900d5ad70
Meghana Madineni <meghanamadineni@gmail.com>
2017-12-03 20:27:09 -0500

    cookies check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { HouseListingService } from './house-listing.service';
 import {AuthenticationService} from '../app/components/services/index'
 import {LoginComponent} from './components/login/login.component'
 import 'rxjs/add/operator/filter';
+import {CookieService} from 'angular2-cookie/core';
 
 import { HttpClient } from '@angular/common/http';
 
@@ -12,7 +13,7 @@ import { HttpClient } from '@angular/common/http';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers:[AuthenticationService]
+  providers:[AuthenticationService, CookieService]
 })
 
 export class AppComponent implements OnInit {
@@ -23,7 +24,8 @@ export class AppComponent implements OnInit {
     sanitizer: DomSanitizer,
     private http: HttpClient,
     private data: HouseListingService,
-    private signout:AuthenticationService
+    private signout:AuthenticationService,
+    private cookie:CookieService
    ) {
       iconRegistry.addSvgIcon( 'logo',
       sanitizer.bypassSecurityTrustResourceUrl('./assets/logo.svg'));
@@ -40,9 +42,8 @@ export class AppComponent implements OnInit {
   }
 
 logout(){
- this.signout.logout();
- document.getElementById("logout").style.visibility = "hidden";
- document.getElementById("login").style.visibility = "visible";
- document.getElementById("signup").style.visibility = "visible";
+  alert('logout')
+  alert(this.cookie.get('loginCookie'))
+  this.cookie.remove('loginCookie')
 }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -13,26 +13,30 @@ from '@angular/router';
 import { HouseListingService } from '../../house-listing.service';
 import {AppComponent} from '../../app.component';
 import { ViewChild } from '@angular/core';
+import {CookieService} from 'angular2-cookie/core';
+import { Cookie } from 'ng2-cookies/ng2-cookies';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  providers: [AuthenticationService]
+  providers: [AuthenticationService, CookieService]
 })
 export class LoginComponent implements OnInit {
  
   constructor(private route: ActivatedRoute,
       private router: Router, private authenticationService: AuthenticationService,
-    private listing: HouseListingService,private app:AppComponent) {}
-
+    private listing: HouseListingService,private app:AppComponent, private cookie:CookieService) {}
   ngOnInit() {
-      this.authenticationService.logout();
+      // this.authenticationService.logout();
   }
 
   loginSubmit(form: any) {    
       this.authenticationService.login(form)
           .subscribe(
               data => {
+                this.cookie.put('loginCookie', 'loginCookie')
+                // this.cookie.setCookie('cookieName', 'cookieValue', 1)
                   this.router.navigate(["/home"]);
                   // console.log(data);
                   // this.listing.userDetail = data;
