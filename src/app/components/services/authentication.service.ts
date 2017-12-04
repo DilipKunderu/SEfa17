b19994b76070353fd3a6cb7fe244963be9ff6745
Meghana Madineni <meghanamadineni@gmail.com>
2017-12-04 16:21:26 -0500

    authentication service update to add cookie

--- src/app/components/services/authentication.service.ts
+++ src/app/components/services/authentication.service.ts
@@ -17,11 +17,12 @@ import {
 from '../models/index';
 import { Router } from '@angular/router';
 import { HouseListingService } from '../../house-listing.service';
+import {CookieService} from 'angular2-cookie/core';
 
 @Injectable()
 export class AuthenticationService {
     constructor(private http: Http, private data: HouseListingService,
-    private router: Router) {}
+    private router: Router, private cookie:CookieService) {}
 
     login(login: Login) {
         return this.http.post('http://192.168.2.24:3000/login', login)
@@ -34,19 +35,11 @@ export class AuthenticationService {
                 this.data.userDetail = b._body;
                 this.data.userName = d[1];
                 this.data.userEmail = d[0];
-
+                this.cookie.put('loginCookie',this.data.userName)
                 let user = response.toString();
-                // console.log(user);
-                localStorage.setItem('currentUser', JSON.stringify(user));
-
                 return user;
             });
     }
 
-    logout() {
 
-        // remove user from local storage to log user out
-        localStorage.removeItem('currentUser');
-        // this.router.navigate(['/']);
-    }
 }
